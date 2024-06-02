6cfcc0ce2c4415be4a7604a32e99602faaa0eb1d
wrg2996 <williamrobert_k_green@homedepot.com>
2024-06-02 13:55:10 -0400

    sorts movie (by IMDB rating for now)

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -1,14 +1,34 @@
-import { Row, Spacer } from "./styled";
+import styled from "styled-components";
+import { Spacer } from "./styled";
 
 const MovieDetails = ({ movie }) => {
+
+    // Ratings
+    // 0: {Source: 'Internet Movie Database', Value: '7.8/10'}
+    // 1: {Source: 'Rotten Tomatoes', Value: '75%'}
+    // 2: {Source: 'Metacritic', Value: '51/100'}
+
     return (
-        <Row>
-            <img width={50} src={movie.Poster} />
-            <h3>{movie.Title} ({movie.Year})</h3>
+        <TabelRow>
+            <BigText>{movie.Title} ({movie.Year})</BigText>
             <Spacer />
-            <h3>{movie.Ratings[0].Value}</h3>
-        </Row>
+            <LittleText>{movie.Ratings[0].Value}</LittleText>
+        </TabelRow>
     )
 }
 
 export default MovieDetails;
+
+const TabelRow = styled.div`
+    display: flex;
+    border-bottom: gray solid;
+    padding: .3em;
+`
+
+const BigText = styled.h3`
+    margin: .1em;
+`
+
+const LittleText = styled.p`
+    margin: .1em;
+`
--- src/components/MovieTable.jsx
+++ src/components/MovieTable.jsx
@@ -2,8 +2,23 @@ import styled from "styled-components";
 import MovieDetails from "./MovieDetails";
 import { Spacer } from "./styled";
 import Search from "./Search";
+import { useEffect, useState } from "react";
 
 const MovieTable = ({ movies }) => {
+
+    const [sortedMovies, setSortedMovies] = useState(movies);
+
+    useEffect(() => {
+        setSortedMovies(movies.sort((a, b) => {
+            const imdbA = +a.info.Ratings
+                .find(r => r.Source = 'Internet Movie Database')
+                .Value.split('/')[0];
+            const imdbB = +b.info.Ratings
+                .find(r => r.Source = 'Internet Movie Database')
+                .Value.split('/')[0];
+            return imdbB - imdbA;
+        }));
+    }, [movies]);
 
     async function saveMovie(movie) {
         let { dataset } = await initThing(
@@ -23,8 +38,11 @@ const MovieTable = ({ movies }) => {
                 <Spacer />
                 <Search select={saveMovie} />
             </Header>
+            <Header>
+
+            </Header>
             {
-                movies.map(m => <MovieDetails key={m.id} movie={m.info} />)
+                sortedMovies.map(m => <MovieDetails key={m.id} movie={m.info} />)
             }
         </Table>
     )
@@ -40,4 +58,6 @@ const Table = styled.div`
 `
 const Header = styled.div`
     display: flex;
+    border-bottom: gray solid;
+    padding: .3em;
 `
